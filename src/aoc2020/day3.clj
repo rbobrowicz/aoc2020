cdafb5206ac1d1b2fbe152d0641c0a574bf8c3a0
radek bobrowicz <radek@bobrowicz.me>
2020-12-06 17:08:56 -0600

    day 3

---
+++ src/aoc2020/day3.clj
@@ -0,0 +1,37 @@
+(ns aoc2020.day3
+  (:require [clojure.string :as str]))
+
+(defn read-input-file [path]
+  (->> (slurp path)
+       (str/split-lines)))
+
+(defn trace-path [terrain x-slope y-slope]
+  (let [path (transient [])]
+    (loop [x 0
+           y-lines terrain]
+      (when-let [y-line (first y-lines)]
+        (conj! path (get y-line (mod x (count y-line))))
+        (recur (+ x x-slope) (nthrest y-lines y-slope))))
+    (persistent! path)))
+
+(defn count-trees [path]
+  (->> path
+       (filter #(= \# %))
+       count))
+
+(defn problem-1 []
+  (let [data (read-input-file "./resources/day3.input")]
+    (count-trees (trace-path data 3 1))))
+
+(defn problem-2 []
+  (let [data (read-input-file "./resources/day3.input")
+        r1d1 (count-trees (trace-path data 1 1))
+        r3d1 (count-trees (trace-path data 3 1))
+        r5d1 (count-trees (trace-path data 5 1))
+        r7d1 (count-trees (trace-path data 7 1))
+        r1d2 (count-trees (trace-path data 1 2))]
+    (* r1d1 r3d1 r5d1 r7d1 r1d2)))
+
+(comment
+  (problem-1)
+  (problem-2))
